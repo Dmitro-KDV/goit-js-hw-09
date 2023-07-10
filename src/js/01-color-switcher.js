f355b19a0147b2faec745a43020997f01fa1567c
Dmitro-KDV <124252345+Dmitro-KDV@users.noreply.github.com>
2023-07-10 13:32:58 +0300

    ДЗ 9 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,41 @@
 
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+const btn = document.querySelectorAll('button');
+const startBtn = document.querySelector('button[data-start]');
+const stopBtn = document.querySelector('button[data-stop]');
+const colorBody= document.querySelector('body');
+
+let timerId = null;
+
+startBtn.addEventListener("click", () => {
+  timerId = setInterval(() => {
+    const backColor = getRandomHexColor();
+    colorBody.style.backgroundColor = backColor;
+    startBtn.setAttribute("disabled", ""); 
+    stopBtn.removeAttribute("disabled");
+  }, 1000);
+});
+
+
+stopBtn.addEventListener("click", () => {
+  clearInterval(timerId);
+  stopBtn.setAttribute("disabled", ""); 
+  startBtn.removeAttribute("disabled");
+});
+
+const firstItem = document.createElement("div");
+firstItem.classList.add('js-btn')
+colorBody.insertBefore(firstItem, colorBody.lastElementChild);
+
+firstItem.append(startBtn, stopBtn);
+const jsBtn = document.querySelector('.js-btn');
+jsBtn.style.textAlign = 'center'
+jsBtn.style.marginTop = '200px'
+startBtn.style.margin = '20px'
+startBtn.style.width = '60px'
+startBtn.style.height = '50px'
+stopBtn.style.width = '60px'
+stopBtn.style.height = '50px'
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,45 @@
+import Notiflix from 'notiflix';
+const form = document.querySelector(".form");
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((res, rej) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+        res(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        rej(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    },delay);
+  });
 }
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const {
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  let timerId = null;
+  let amountInter = amount.value;
+  let positionP = 1;
+  let delayP = Number(delay.value);
+  timerId = setInterval(() => {
+    amountInter -=1;
+    createPromise(positionP, delayP)
+    .then((value) => {
+      Notiflix.Notify.success(value,{timeout: 4000,},);
+    })
+    .catch((value) => {
+      Notiflix.Notify.failure(value,{timeout: 4000,},);
+    });
+
+    positionP +=1;
+    delayP += Number(step.value);
+
+    if (!amountInter) {
+        clearInterval(timerId);
+    }
+  }, 1000);
+
+});
